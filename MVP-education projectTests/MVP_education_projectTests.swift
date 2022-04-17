5a1dc7c2598ade9eff3781a1553997ce78a60d6f
Maxim <swiftdevfromsiberia@gmail.com>
2022-04-18 00:24:30 +0500

    Person view added and covered with tests

--- MVP-education projectTests/MVP_education_projectTests.swift
+++ MVP-education projectTests/MVP_education_projectTests.swift
@@ -8,29 +8,43 @@
 import XCTest
 @testable import MVP_education_project
 
+class MockView: MainViewProtocol {
+    var titleTest: String?
+    func setGreeting(greeting: String) {
+        self.titleTest = greeting
+    }
+}
+
+var view: MockView!
+var person: Person!
+var presenter: MainPresenter!
+
+ 
 class MVP_education_projectTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        view = MockView()
+        person = Person(firstName: "Baz", lastName: "Bar")
+        presenter = MainPresenter(view: view, person: person)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        view = nil
+        person = nil
+        presenter = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testModuleIsNotNil() {
+        XCTAssertNotNil(view, "view is not nil")
+        XCTAssertNotNil(person, "person is not nil")
+        XCTAssertNotNil(presenter, "presenter is not nil")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testView() {
+        presenter.showGreeting()
+        XCTAssertEqual(view.titleTest, "Baz Bar")
     }
+    func testPersonModel() {
+        XCTAssertEqual(person.firstName, "Baz")
+        XCTAssertEqual(person.lastName, "Bar")
 
+    }
 }
