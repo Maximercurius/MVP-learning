d0abfaa617cdc05497d61060dac87818cf2947db
Maxim <swiftdevfromsiberia@gmail.com>
2022-04-17 23:01:30 +0500

    Initial Commit

---
+++ MVP-education project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP-education project
+//
+//  Created by Makarov_Maxim on 17.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
